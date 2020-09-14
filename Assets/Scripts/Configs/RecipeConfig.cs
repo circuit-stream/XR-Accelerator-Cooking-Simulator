6be77721c7fe77b74ac529dfa6d92e19c670b787
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2020-09-14 10:54:14 -0300

    Configs provider + recipe config

--- Assets/Scripts/Configs/RecipeConfig.cs
+++ Assets/Scripts/Configs/RecipeConfig.cs
@@ -1,22 +1,45 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using XRAccelerator.Enums;
+using XRAccelerator.Gameplay;
 
 namespace XRAccelerator.Configs
 {
     [CreateAssetMenu(fileName = "New Recipe Config", menuName = "Configs/Recipe", order = 0)]
     public class RecipeConfig : ScriptableObject
     {
-        public struct IngredientRequirement
+        [Serializable]
+        public class IngredientRequirement
         {
             public IngredientConfig Ingredient;
-            public int MinAmount;
-            public int MaxAmount;
+            public float MinAmount;
+            public float MaxAmount;
         }
 
+        [SerializeField]
         public List<IngredientRequirement> SourceIngredients;
-
+        [SerializeField]
         public ApplianceType ApplianceType;
+        [SerializeField]
         public IngredientConfig OutputIngredient;
+
+        public bool DoesIngredientsSatisfyRecipe(List<IngredientAmount> ingredients)
+        {
+            if (ingredients.Count != SourceIngredients.Count)
+                return false;
+
+            foreach (var sourceIngredient in SourceIngredients)
+            {
+                var ingredientAmount = ingredients.Find(possibleIngredient => possibleIngredient.Ingredient == sourceIngredient.Ingredient);
+
+                if (ingredientAmount == null ||
+                    sourceIngredient.MaxAmount < ingredientAmount.Amount ||
+                    sourceIngredient.MinAmount > ingredientAmount.Amount)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
