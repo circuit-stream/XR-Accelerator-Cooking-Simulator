b7a61fbf62d613bc79846a25eb444a4e000a66db
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2020-09-14 18:14:13 -0300

    Inicial stewPan logic

--- Assets/Scripts/Gameplay/Appliances/StewPan.cs
+++ Assets/Scripts/Gameplay/Appliances/StewPan.cs
@@ -5,7 +5,15 @@ namespace XRAccelerator.Gameplay
 {
     public class StewPan : Container
     {
+        // TODO Arthur Optional: Heat temperature, stirring velocity, cook time per recipe
+        private const float cookTime = 5;
+        private const float minStirringTime = 0;
+
+        [SerializeField]
+        private IngredientGraphics burntIngredientPrefab;
+
         private float applianceEnabledTime;
+        private float stirringTime;
 
         private void EnableAppliance()
         {
@@ -17,29 +25,82 @@ namespace XRAccelerator.Gameplay
         {
             isApplianceEnabled = false;
         }
-        
+
+        private void ExecuteRecipe()
+        {
+            DestroyCurrentIngredients();
+            CreateIngredient(GetOutputIngredient());
+
+            applianceEnabledTime = 0;
+            stirringTime = 0;
+        }
+
+        private IngredientGraphics GetOutputIngredient()
+        {
+            if (stirringTime < minStirringTime || CurrentRecipeConfig == null)
+            {
+                // TODO Arthur Optional: Have different prefab for burnt / wrong ingredients
+                return burntIngredientPrefab;
+            }
+
+            return CurrentRecipeConfig.OutputIngredient.IngredientPrefab;
+        }
+
+        private void DestroyCurrentIngredients()
+        {
+            foreach (var ingredientGraphics in CurrentIngredientGraphics)
+            {
+                Destroy(ingredientGraphics.gameObject);
+            }
+
+            CurrentIngredientGraphics.Clear();
+            CurrentIngredients.Clear();
+        }
+
+        private void CreateIngredient(IngredientGraphics prefab)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+
         protected override void OnTriggerEnter(Collider other)
         {
+            var stoveFire = other.gameObject.GetComponent<StoveFire>();
+            if (stoveFire != null)
+            {
+                EnableAppliance();
+                return;
+            }
+
+            // TODO Arthur: If spoon Enable Stirring
+
             base.OnTriggerEnter(other);
-            
-            // TODO Arthur: If Fire call EnableAppliance
         }
 
         protected override void OnTriggerExit(Collider other)
         {
+            var stoveFire = other.gameObject.GetComponent<StoveFire>();
+            if (stoveFire != null)
+            {
+                DisableAppliance();
+                return;
+            }
+
             base.OnTriggerExit(other);
-            
-            // TODO Arthur: If Fire call DisableAppliance
+
+            // TODO Arthur: If spoon Disable Stirring
         }
 
         private void Update()
         {
             if (!isApplianceEnabled)
                 return;
-            
-            // TODO Arthur: After currentRecipeTime execute recipe
-            
-            throw new NotImplementedException();
+
+            applianceEnabledTime += Time.deltaTime;
+
+            if (applianceEnabledTime > cookTime)
+            {
+                ExecuteRecipe();
+            }
         }
     }
 }
