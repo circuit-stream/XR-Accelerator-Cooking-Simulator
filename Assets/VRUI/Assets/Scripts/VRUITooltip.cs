005228ce4b93da9af62c172c2cb93830cd524817
Jerry Medeiros <jerrylewisbh@gmail.com>
2020-10-25 21:34:24 -0300

    adding ui samples

---
+++ Assets/VRUI/Assets/Scripts/VRUITooltip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+// VRUI Tooltip Component
+namespace XRAccelerator.VRUI
+{
+    [ExecuteAlways]
+    public class VRUITooltip : MonoBehaviour
+    {
+        [SerializeField] 
+        private Transform container;
+        [SerializeField]
+        private Transform anchor;
+        [SerializeField] 
+        private LineRenderer link;
+
+        private void Start()
+        {
+            link.positionCount = 2;
+        }
+
+        // Update is called once per frame
+        protected virtual void Update()
+        {
+            var points = new Vector3[link.positionCount];
+            points[0] = container.position;
+            points[1] = anchor.position;
+            link.SetPositions(points);
+        }
+    }
+}
